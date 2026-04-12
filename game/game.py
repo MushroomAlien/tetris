689895b3504eb057dd25001cb8ac72d98b0d81e7
mushroomalien <mushroomalien@gmail.com>
2026-04-12 21:56:29 +1000

    Fix: Remove duplicate class definition from game/game.py

--- game/game.py
+++ game/game.py
@@ -25,4 +25,17 @@ class Game:
         self.frame_counter += 1
         if self.frame_counter >= constants.FALL_INTERVAL:
             self.frame_counter = 0
+            self._fall_piece()
+
+    def _fall_piece(self):
+        current_cells = self.get_current_cells()
+        next_cells = [(r + 1, c) for r, c in current_cells]
+
+        if self.board.is_valid_position(next_cells):
             self.row += 1
+        else:
+            # Collision detected: The piece has landed!
+            self.board.place_piece(current_cells, self.piece_id)
+            lines_cleared = self.board.clear_lines()
+            self.score += lines_cleared
+            self.spawn_piece()
