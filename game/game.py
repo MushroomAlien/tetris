7fd57c3a33f0f4e767602a5b0c11f7edb513be38
mushroomalien <mushroomalien@gmail.com>
2026-04-14 23:50:52 +1000

    Add handle_action to Game, add action constants to constants.py
    - Added ACTION_NONE through ACTION_QUIT constants to constants.py
    - Added handle_action(self, action) method to game.py
    - Handles MOVE_LEFT, MOVE_RIGHT, ROTATE, SOFT_DROP
    - Known issues: spawn_piece missing from _fall_piece landing branch, rotation validity check missing row/col offset

--- game/game.py
+++ game/game.py
@@ -38,4 +38,26 @@ class Game:
             self.board.place_piece(current_cells, self.piece_id)
             lines_cleared = self.board.clear_lines()
             self.score += lines_cleared
-            self.spawn_piece()
+    def handle_action(self, action):
+        # Check action against constants module
+        if action == constants.ACTION_MOVE_LEFT:
+            # Calculate potential new cell positions by moving one column left
+            potential_cells = [(r, c - 1) for r, c in self.get_current_cells()]
+            if self.board.is_valid_position(potential_cells):
+                self.col -= 1
+        elif action == constants.ACTION_MOVE_RIGHT:
+            # Calculate potential new cell positions by moving one column right
+            potential_cells = [(r, c + 1) for r, c in self.get_current_cells()]
+            if self.board.is_valid_position(potential_cells):
+                self.col += 1
+        elif action == constants.ACTION_ROTATE:
+            # Calculate potential cells after rotating 90 degrees clockwise
+            next_rotation = (self.rotation + 1) % 4
+            potential_cells = [(r, c) for r, c in tetromino.get_shape(self.piece_id, next_rotation)]
+            if self.board.is_valid_position(potential_cells):
+                self.rotation = next_rotation
+        elif action == constants.ACTION_SOFT_DROP:
+            # Calculate potential cells by dropping one row
+            potential_cells = [(r + 1, c) for r, c in self.get_current_cells()]
+            if self.board.is_valid_position(potential_cells):
+                self.row += 1
